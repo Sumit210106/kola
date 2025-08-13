9691c61d9ce15ff63f07dd1d262b7033c4c29225
sumit Nayak <sumitnayak2006@gmail.com>
2025-08-14 01:14:48 +0530

    update of error

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import logo from "@/public/kola-logo.png"; \
+import logo from "@/public/kola-logo.png"; 
 import {
   Mail,
   Phone,
